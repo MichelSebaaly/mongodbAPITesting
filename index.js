cf06e41d97ae0b948ebbcccc30c953a3edbf5b92
Michel Sebaaly <michelsebaaly4@gmail.com>
2025-04-30 20:39:19 +0300

    C3

--- index.js
+++ index.js
@@ -52,6 +52,6 @@ connectDB().then((collection) => {
   studentsCollection = collection;
   const PORT = process.env.PORT || 3000;
   app.listen(PORT, () => {
-    console.log("Server is running on port 3000");
+    console.log(`Server is running on port: ${PORT}`);
   });
 });
